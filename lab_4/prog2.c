7645d1a2db9e16bc4e6312be5ea7246625b342c5
mnsir <mnsir@ya.ru>
2021-04-03 22:30:43 +0300

    if -> switch

--- lab_4/prog2.c
+++ lab_4/prog2.c
@@ -8,48 +8,48 @@
 
 #include "sleep.h"
 
+#define EXEC "./prog1"
+
 extern int errno;
 
 int main(int argc, char *argv[])
 {
 	int child_retval;
-	
+
 	printf("%s: ", argv[0]);
 	printf("process id: %08X, parent process id: %08X\n", getpid(), getppid());
-	
+
 	pid_t pid = fork();
 	/* если внезапно ошибка */
-	if (pid < 0)
+	switch (pid)
 	{
+	case -1:
 		/* TODO обработать */
 		return 1;
-	}
-	if (pid == 0)
-	{
+	case 0:
 		/* дочерний процесс */
 		printf("%s: ", argv[0]);
 		printf("child process id: %08X\n", getpid());
-		const char* executable = "./prog1";
-		execle(executable, executable, "qwe", "asd", "zxc", NULL, NULL);
+		execle(EXEC, EXEC, "qwe", "asd", "zxc", NULL, NULL);
 		return 13;
-	}
-	do
-	{
-		sleep_ms(500);
-		printf("%s: ", argv[0]);
-		printf("child process still works\n");
+	default:
+		do
+		{
+			sleep_ms(500);
+			printf("%s: ", argv[0]);
+			printf("child process still works\n");
 
-	} while (waitpid(pid, &child_retval, WNOHANG) != pid);
-	if (WIFSIGNALED(child_retval))
-	{
-		printf("%s: ", argv[0]);
-		printf("child process was terminated by a signal %d.\n", WTERMSIG(child_retval));
+		} while (waitpid(pid, &child_retval, WNOHANG) != pid);
+		if (WIFSIGNALED(child_retval))
+		{
+			printf("%s: ", argv[0]);
+			printf("child process was terminated by a signal %d.\n", WTERMSIG(child_retval));
+		}
+		else if (WIFEXITED(child_retval))
+		{
+			printf("%s: ", argv[0]);
+			printf("child process terminated normally with code %d.\n", WEXITSTATUS(child_retval));
+		}
+		return 0;
 	}
-	else if (WIFEXITED(child_retval))
-	{
-		printf("%s: ", argv[0]);
-		printf("child process terminated normally with code %d.\n", WEXITSTATUS(child_retval));
-	}
-	
-	return 0;
 }
